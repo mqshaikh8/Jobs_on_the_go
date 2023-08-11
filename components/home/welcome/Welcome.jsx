8842f1cf4e6d4066637091057d53cc5f3681ccde
mqshaikh8 <mqshaikh8@gmail.com>
2023-08-11 11:04:13 +0530

    figuring out the fetch

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,9 +11,13 @@ import { useRouter } from 'expo-router';
 import { icons, SIZES } from '../../../constants';
 import styles from './welcome.style'
 
+const jobTypes = [
+  "Full-time", "Part-time", "Freelance", "Internship", "Temporary", "Contract", "Commission", "Volunteer"
+];
+
 const Welcome = () => {
   const router = useRouter();
-
+  const [activeJobType, setActiveJobType] = useState('Full-time');
   return (
     <View>
       <View style={styles.container}>
@@ -36,6 +40,26 @@ const Welcome = () => {
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => {
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          }}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
       </View>
     </View>
   )
